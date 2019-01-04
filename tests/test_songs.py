0bb56bc7be6e70097d38684d4a0cf428146eeac9
David Ben-Ner <david.benner@gmail.com>
2019-01-04 13:21:35 +0200

    Changed to package structure and added tests

---
+++ tests/test_songs.py
@@ -0,0 +1,13 @@
+from songs import babyshark, wheels, knees_toes
+
+
+def test_baby_shark():
+    babyshark.song()
+
+
+def test_knees_toes():
+    knees_toes.song()
+
+
+def test_wheels():
+    wheels.song()
